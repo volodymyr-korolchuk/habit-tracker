4f4ed89a26202f39f802d17417fe66eec1d7f4a5
Volodymyr Korolchuk <gitpub10@gmail.com>
2024-03-06 22:15:25 +0200

    NumberTile removed

--- app/tracker/page.tsx
+++ app/tracker/page.tsx
@@ -41,9 +41,11 @@ const TrackerPage = () => {
 
   const daysOfMonthArray = (
     <>
-      {daysOfMonth.map((item, index) => {
-        return <NumberTile key={index} value={index + 1} />;
-      })}
+      {daysOfMonth.map((item, index) => (
+        <div className="flex items-center justify-center md:w-16 w-12 md:h-full h-12 bg-neutral-300 rounded-sm text-[30px]">
+          <p>{index + 1}</p>
+        </div>
+      ))}
     </>
   );
 
