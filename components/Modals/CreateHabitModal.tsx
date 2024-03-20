0f5d07c4c2e42bd090f7591b42845ddfbab13009
Volodymyr Korolchuk <gitpub10@gmail.com>
2024-03-20 13:02:57 +0200

    ui: create habit modal appearance tweaked

--- components/Modals/CreateHabitModal.tsx
+++ components/Modals/CreateHabitModal.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import toast from "react-hot-toast";
 import { FaXmark } from "react-icons/fa6";
 import { Input } from "@/components/ui/input";
+import { Label } from "../ui/label";
+import { Button } from "../ui/button";
 
 interface Props {
   isOpened: boolean;
@@ -13,6 +15,7 @@ const isValidInput = (title: string) => {
     toast.error("Title was not provided");
     return false;
   }
+  return true;
 };
 
 const CreateHabitModal: React.FC<Props> = ({ isOpened, onClose }) => {
@@ -47,39 +50,41 @@ const CreateHabitModal: React.FC<Props> = ({ isOpened, onClose }) => {
           "isOpened flex items-center justify-center absolute top-0 left-0 w-screen h-screen z-50 backdrop-blur-sm bg-neutral-900/80"
         }
       >
-        <section className="flex flex-col items-center justify-center bg-blue-200 border-2 border-neutral-800 rounded-xl p-2 group">
-          <div className="w-full text-end flex items-center justify-between pb-2 pl-2">
-            <p className="text-4xl">Create a new Habit!</p>
-            <button onClick={onClose} className="hover:bg-rose-400 rounded-md">
-              <FaXmark size={45} />
+        <section className="flex flex-col items-center bg-neutral-100 backdrop-blur-xl rounded-xl p-3">
+          <div className="w-full flex items-center justify-between pb-2 pl-1 gap-2">
+            <p className="text-3xl">Create a new Habit!</p>
+            <button onClick={onClose} className="hover:bg-red-500 rounded-md">
+              <FaXmark size={35} />
             </button>
           </div>
 
           <form
             id="new-habit-form"
-            className="flex gap-2"
+            className="flex flex-col gap-2 w-full"
             onSubmit={handleSubmit}
           >
-            <Input
-              type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              placeholder="Title"
-              maxLength={20}
-            />
-
-            <button
-              form="new-habit-form"
+            <div>
+              <Label htmlFor="habit-title" className="text-md">
+                Title
+              </Label>
+              <Input
+                value={title}
+                onChange={(e) => {
+                  setTitle(e.target.value);
+                }}
+                id="habit-title"
+                className="h-14 text-xl shadow-none border-[1px] border-neutral-500"
+                maxLength={28}
+              />
+            </div>
+            <Button
               type="submit"
-              className="bg-green-400 hover:bg-green-200 w-32 text-2xl rounded-md"
+              form="new-habit-form"
+              className="bg-green-400 hover:bg-green-200 w-full h-14 p-2 px-4 text-2xl rounded-md"
             >
               Create
-            </button>
+            </Button>
           </form>
-          <p className="pt-2 px-2 text-start w-full group-hover:opacity-100 opacity-0 transition-all text-neutral-700">
-            *Choose the title wisely. It won`t be possible to change it later{" "}
-            {":)"}
-          </p>
         </section>
       </div>
     )
