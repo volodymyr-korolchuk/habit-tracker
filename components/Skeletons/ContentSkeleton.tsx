41ad05c0b4554322a8c9905587ed272ab254b665
Volodymyr Korolchuk <gitpub10@gmail.com>
2024-03-22 00:49:25 +0200

    chore: unused components removed

--- components/Skeletons/ContentSkeleton.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-const ContentSkeleton = () => {
-  return <div className="shimmer h-[205px]"></div>;
-};
-
-export default ContentSkeleton;
--- components/Skeletons/DaysOfMonthSkeleton.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-const DaysOfMonthSkeleton = () => {
-  return <div className="flex-1 h-full rounded-lg shimmer"></div>;
-};
-
-export default DaysOfMonthSkeleton;
